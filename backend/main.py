4dd0f23eecc1aae8705a571d608e2bf058edb321
tejoess <tejassutarofc@gmail.com>
2026-02-21 17:08:59 +0530

    first commit

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,23 @@
-# app/main.py
+# backend/main.py
 from fastapi import FastAPI , WebSocket
-from app.schemas.request import PaperGenerationRequest
-from app.schemas.response import PaperGenerationResponse
-from app.services.pipeline import run_question_paper_pipeline
-from app.websocket.manager import manager
+from backend.schemas.request import PaperGenerationRequest
+from backend.schemas.response import PaperGenerationResponse
+from backend.services.pipeline import run_question_paper_pipeline
+from backend.websocket.manager import manager
 
 
-app = FastAPI()
+backend = FastAPI()
 
 
 
-@app.websocket("/ws/{session_id}")
+@backend.websocket("/ws/{session_id}")
 async def websocket_logs(websocket: WebSocket, session_id: str):
     await manager.connect(session_id, websocket)
     while True:
         await websocket.receive_text()
 
 
-@app.post("/generate-paper", response_model=PaperGenerationResponse)
+@backend.post("/generate-paper", response_model=PaperGenerationResponse)
 def generate_question_paper(payload: PaperGenerationRequest):
     session_id = "session_1"
     file_path = run_question_paper_pipeline(session_id)
